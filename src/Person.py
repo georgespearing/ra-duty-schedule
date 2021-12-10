1567bd5632331279b1fafef8cfe8752c5f38bb6c
George Spearing <spearingg@gmail.com>
2021-12-10 11:13:10 -0500

    adding two fitness parameters

--- src/Person.py
+++ src/Person.py
@@ -1,8 +1,9 @@
 
 class Person:
 
-    def __init__(self, name): 
+    def __init__(self, name, building): 
         self.name = name
+        self.building = building
         self.day_preference = [] # M -> Su for preference of days
         self.no_go_days = [] # date as string "05/03/21" that are no go
         self.active_shifts = [] # date as string "05/03/21" that are assigned
@@ -10,7 +11,8 @@ class Person:
         self.days_active = 0
         self.days_primary = 0
         self.days_since_last_duty = 0
-        self.fitness = 0
+        self.fitness_primary = 0
+        self.fitness_secondary = 0
 
     # def __init__(self, name, days, no_go):
     #     self.__init__()
@@ -18,7 +20,7 @@ class Person:
     #     self.add_no_go(no_go)
 
     def add_shift(self, shift):
-        self.active_shifts.append(s)
+        self.active_shifts.append(shift)
 
     def add_no_go(self, date):
         self.no_go_days.append(date)
@@ -32,8 +34,8 @@ class Person:
         self.no_go_days = []
         self.active_shifts = []
 
-    def to_string(self):
+    def string_person(self):
         return f'{self.name}; Prefers: {self.day_preference}; Cannot work {self.no_go_days}'
 
-    def results_string(self):
+    def string_results(self):
         return f'{self.name}; Primary Shifts: {self.days_primary}; Secondary Shifts: {self.days_active-self.days_primary}'
